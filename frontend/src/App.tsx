381aca0bf86fb213911af10f4676ec0dfc892cd9
Gyt-18h <arusutoromeria418@gmail.com>
2026-07-05 01:52:28 +0900

    feat: App.tsxにWebプロフィール画面の読み込み追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import HomePage from './pages/MapPage';
 import SpotPostPage from './pages/SpotPostPage';
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <Route path="/" element={<Navigate to="/map" replace />} />
           <Route path="/map" element={<HomePage />} />
           <Route path="/post" element={<SpotPostPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
         </Routes>
       </BrowserRouter>
     </div>
