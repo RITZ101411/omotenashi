29d5e279ced4e10c16b60be1dea156200df52064
RITZ_ <ritz1014.ritz1014@gmail.com>
2026-07-04 23:53:44 +0900

    fix: タブ遷移のアニメーション無効化、足あとフローだけアニメ残す

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -7,14 +7,14 @@ export default function RootLayout() {
   return (
     <React.Fragment>
       <StatusBar style="auto" />
-      <Stack screenOptions={{ headerShown: false }}>
+      <Stack screenOptions={{ headerShown: false, animation: "none" }}>
         <Stack.Screen name="index" />
         <Stack.Screen name="post" />
         <Stack.Screen name="mypage" />
-        <Stack.Screen name="spot/[id]" />
+        <Stack.Screen name="spot/[id]" options={{ animation: "slide_from_bottom" }} />
         <Stack.Screen name="spot/camera" options={{ presentation: "fullScreenModal" }} />
-        <Stack.Screen name="spot/reaction" />
-        <Stack.Screen name="spot/complete" />
+        <Stack.Screen name="spot/reaction" options={{ animation: "slide_from_right" }} />
+        <Stack.Screen name="spot/complete" options={{ animation: "fade" }} />
         <Stack.Screen name="components-demo" />
       </Stack>
     </React.Fragment>
